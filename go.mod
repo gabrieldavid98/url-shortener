e9b3727e5ecd7916464b003c1caaca3c14c686f7
david <david@debian.net>
2019-11-05 23:03:39 -0500

    Json and Msgpack serializers implementation added

--- go.mod
+++ go.mod
@@ -3,7 +3,16 @@ module github.com/ghostthrone/url-shortener
 go 1.13
 
 require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.1.3
+	golang.org/x/crypto v0.0.0-20191105034135-c7e5f84aec59 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/dealancer/validate.v2 v2.1.0
 )
---
+++ repository/mongodb/repository.go
@@ -0,0 +1,39 @@
+package mongodb
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type mongoRepository struct {
+	client   *mongo.Client
+	database string
+	timeout  time.Duration
+}
+
+func newMongoClient(mongoURL string, mongoTimeout int) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Duration(mongoTimeout)*time.Second,
+	)
+
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	return client, err
+}
